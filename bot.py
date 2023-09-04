53cdb2c4be875031e70a5276c2cdd0d8c53a279f
Hodakov <teeror93@gmail.com>
2023-09-04 20:38:59 +0400

    Получение планет при помощи getattr

--- bot.py
+++ bot.py
@@ -34,36 +34,15 @@ def talk_to_me(update, context):
 def get_planet(update, context):
     user_text = update.message.text
     today_data = datetime.date.today()
-    planet_input = user_text.split()[-1].lower()
-
-    planet = {
-        'jupiter': ephem.Jupiter(today_data),
-        'mars': ephem.Mars(today_data),
-        'mercury': ephem.Mercury(today_data),
-        'moon': ephem.Moon(today_data),
-        'neptune': ephem.Neptune(today_data),
-        'saturn': ephem.Saturn(today_data),
-        'sun': ephem.Sun(today_data),
-        'uranus': ephem.Uranus(today_data),
-        'venus': ephem.Venus(today_data),
-
-        'юпитер': ephem.Jupiter(today_data),
-        'марс': ephem.Mars(today_data),
-        'меркурий': ephem.Mercury(today_data),
-        'луна': ephem.Moon(today_data),
-        'нептун': ephem.Neptune(today_data),
-        'сатурн': ephem.Saturn(today_data),
-        'солнце': ephem.Sun(today_data),
-        'уран': ephem.Uranus(today_data),
-        'венера': ephem.Venus(today_data),
-    }
-
-    if planet_input in planet:
-        constellation = ephem.constellation(planet[planet_input])
+    planet_input = user_text.split()[-1].capitalize()
+    try:
+        planet = getattr(ephem,planet_input)(today_data)
+        constellation = ephem.constellation(planet)
         update.message.reply_text(f'{planet_input.capitalize()} находиться в {constellation[1]} созвездии')
-    else:
+    except AttributeError:
         update.message.reply_text(f'Вы ввели недопустимое значение. Выберите допустимое из списка ниже:'
-                                  f'\nСолнце, Меркурий, Венера, Луна, Марс, Юпитер, Сатурн, Уран, Нептун')
+                                  f'\nСолнце, Меркурий, Венера, Луна, Марс, Юпитер, Сатурн, Уран, Нептун'
+                                  f'\nЗапрос пишетcя на английском языке')
 
 
 if __name__ == "__main__":
